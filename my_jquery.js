e41e4a2cbc4083f67c1b76f4baa83319e7546def
Ryan Chiang <chiang.ryan@hotmail.com>
2022-11-22 18:46:53 -0800

    Week 13 Update

--- my_jquery.js
+++ my_jquery.js
@@ -16,6 +16,10 @@ $(document).ready(function () {
         parallax();
     });
 
+    document.getElementById("problem").addEventListener('scroll', function(e) {
+        parallax();
+    })
+
     function changeNavbar() {
         var navbar = $(".navbar-detect");
         var viewHeight = window.innerHeight;
@@ -28,7 +32,7 @@ $(document).ready(function () {
 
     function teamTxtFlyIn() {
         var navbar = $(".team-item");
-        var ctnNum = 5;
+        var ctnNum = 6.5;
         var viewHeightTeam = window.innerHeight * ctnNum;
         if ($(this).scrollTop() >= viewHeightTeam*((ctnNum-0.5)/ctnNum)) {
             navbar.addClass("visible").removeClass("hidden");
@@ -37,6 +41,7 @@ $(document).ready(function () {
         }
     }
 
+    /*
     function heroTxtFlyIn() {
         var navbar = $(".hero-hook");
         var viewHeight = window.innerHeight;
@@ -46,23 +51,30 @@ $(document).ready(function () {
             navbar.removeClass("hidden").addClass("visible");
         }
     }
+    */
 
     function parallax() {
         var viewHeight = window.innerHeight;
         var descImgCtn = 2;
+        var heroScroll = $(this).scrollTop()*0.4 + "px"
+        document.getElementById("hero-ctn-1").style.backgroundPositionY = heroScroll;
         if ($(this).scrollTop() <= viewHeight*2) {
-            var heroScroll = $(this).scrollTop()*0.4 + "px"
-            document.getElementById("hero-ctn-1").style.backgroundPositionY = heroScroll;
         }
+        /*
+        var xScroll = $("#problem").scrollLeft()*-0.4 + "px"
+        document.getElementById("problem").style.backgroundPositionX = xScroll;
+        console.log(xScroll)
+        */
         /*
         var descImgScroll = toString(((viewHeight*descImgCtn)-$(this).scrollTop())) + "px"
         document.getElementById("desc-img-1").style.transform = "translateY(%s)",descImgScroll;
         */
     }
 
+    
     function scrollbar() {
         var viewHeight = window.innerHeight;
-        var ctnNum = 5;
+        var ctnNum = 3;
         var percentHeight = ($(this).scrollTop() / (viewHeight*ctnNum))*100;
         var percentHeight = percentHeight.toString()+"%";
         document.getElementById("scrollbar").style.width = percentHeight;
